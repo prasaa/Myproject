04638e952061657abd206bbb1fec23e3108affc2
Prashamsha Maharjan <prashamsha.maharjan@f1soft.com>
2022-10-31 14:06:46 +0545

    login page, dashboard , forms are design

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+//import { DashboardComponent } from './dashboard/dashboard.component';
+// import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    routingComponents,
   ],
   imports: [
     BrowserModule,
